21a3c4fa1158a8be3e183536a5af974088beb516
Erick Martinez <erickmartinezhuaylla123@gmail.com>
2022-02-24 22:36:24 -0400

    RestAssured- CRUD Item

---
+++ src/test/java/tareaCRUDItem/CRUDItemTarea.java
@@ -0,0 +1,74 @@
+package tareaCRUDItem;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CRUDItemTarea {
+    @Test
+    public void crudProject(){
+        //Create
+        JSONObject body = new JSONObject();
+        body.put("Content","ErickJSON");
+
+        Response response=given()
+                .auth()
+                .preemptive()
+                .basic("upb_api@api.com","12345")
+                .body(body.toString())
+                .log().all()
+        .when()
+                .post("https://todo.ly/api/items.json");
+
+        response.then()
+                .statusCode(200)
+                .body("Content",equalTo("ErickJSON"))
+                .log().all();
+
+        int idProject =response.then().extract().path("Id");
+
+        // Read
+        response=given()
+                .auth()
+                .preemptive()
+                .basic("upb_api@api.com","12345")
+                .log().all()
+        .when()
+                .get("https://todo.ly/api/items.json");
+
+        response.then()
+                .statusCode(200)
+                .log().all();
+        // Update
+        body.put("Content","ErickUpdate");
+        response=given()
+                .auth()
+                .preemptive()
+                .basic("upb_api@api.com","12345")
+                .body(body.toString())
+                .log().all()
+        .when()
+                .put("https://todo.ly/api/items/"+idProject+".json");
+
+        response.then()
+                .statusCode(200)
+                .body("Content",equalTo("ErickUpdate"))
+                .log().all();
+        // Delete
+        response=given()
+                .auth()
+                .preemptive()
+                .basic("upb_api@api.com","12345")
+                .log().all()
+        .when()
+                .delete("https://todo.ly/api/items/"+idProject+".json");
+
+        response.then()
+                .statusCode(200)
+                .body("Content",equalTo("ErickUpdate"))
+                .body("Deleted",equalTo(true))
+                .log().all();
+    }
+}
